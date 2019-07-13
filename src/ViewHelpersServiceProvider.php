e045bfcfb13b0887292d0999f1e1523c4452e108
Dean Blackborough <dean@g3d-development.com>
2019-07-13 01:32:32 +0100

    Add publishes

--- src/ViewHelpersServiceProvider.php
+++ src/ViewHelpersServiceProvider.php
@@ -9,6 +9,10 @@ class ViewHelpersServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'viewhelpers');
+
+        $this->publishes([
+            __DIR__ . '/resources/views/' => resource_path('views/vendor/viewhelpers')
+        ]);
     }
 
     public function register()
