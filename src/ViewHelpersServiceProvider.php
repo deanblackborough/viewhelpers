7d54c21fb68b7d19711265433a9303ce0fd7ef44
Dean Blackborough <dean@g3d-development.com>
2019-07-13 15:40:48 +0100

    Change alias
    - Change alias

--- src/ViewHelpersServiceProvider.php
+++ src/ViewHelpersServiceProvider.php
@@ -8,10 +8,10 @@ class ViewHelpersServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'viewhelpers');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'view-helpers');
 
         $this->publishes([
-            __DIR__ . '/resources/views' => resource_path('views/vendor/viewhelpers')
+            __DIR__ . '/resources/views' => resource_path('views/vendor/view-helpers')
         ]);
     }
 
